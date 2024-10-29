b3ba38cf5ccc36c59f6a7b7c192e1126c16d5be7
Yuvika Dube <yuvikadube@gmail.com>
2024-10-29 12:17:50 -0700

    Acceleration

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.timer = 0
+        self.speed = PLAYER_MOVE_SPEED
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
@@ -23,18 +24,23 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        else:
+            self.speed = PLAYER_MOVE_SPEED
         if keys[pygame.K_SPACE]:
             if self.timer <= 0:
                 self.shoot()
                 self.timer = PLAYER_SHOOT_COOLDOWN
+        
 
     def move(self, dt):
+        if self.speed < PLAYER_MOVE_SPEED_MAX:
+            self.speed += (200 * dt)
         # start with a unit vector pointing from 0,0 to 0,1
         # rotate that vector by the player's rotation
         # multiply by speed * dt so it's constant across machines
         # add it to our position to move the player
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_MOVE_SPEED * dt
+        self.position += forward * self.speed * dt
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
